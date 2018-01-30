976acb69250263960dfd05c64443759f15ccff98
Unknown <sirindawisetchat@gmail.com>
2018-01-29 19:50:41 -0600

    Week 1: Add state tax source code and log

---
+++ Week 1/StateTax.py
@@ -0,0 +1,12 @@
+#my first program work
+#hello class
+
+print("Sirinda Wisetchat")
+print("Sales Tax Rates")
+print("_______________")
+print("Kansas:  "),  6.5,("%")
+print("Iowa:    "),  6.0,("%")
+print("Hawii:   "),  4.0,("%")
+print("Colorado:"),  2.9,("%")
+print("Michigan:"),  6.0,("%")
+input()
